a7622c77702fef87caef9357c231e042681f1caf
tomaszzywicki <tomasz.zywicki03@gmail.com>
2025-05-11 20:20:20 +0200

    add scd2 to dim_person

--- create_dwh.sql
+++ create_dwh.sql
@@ -1,3 +1,90 @@
+-- dim_crash_info
+CREATE TABLE
+    dim_crash_info (
+        crash_info_id BIGINT NOT NULL PRIMARY KEY,
+        traffic_control_device NVARCHAR (50) NOT NULL,
+        device_condition NVARCHAR (50) NOT NULL,
+        first_crash_type NVARCHAR (50) NOT NULL,
+        trafficway_type NVARCHAR (50) NOT NULL,
+        alignment NVARCHAR (50) NOT NULL,
+        roadway_surface_condition NVARCHAR (50) NOT NULL,
+        road_defect NVARCHAR (50) NOT NULL,
+        report_type NVARCHAR (50) NULL,
+        -- w sensie jest troche missing to null ?
+        crash_type NVARCHAR (50) NOT NULL,
+        damage NVARCHAR (50) NOT NULL,
+        prime_contributory_cause NVARCHAR (50) NOT NULL,
+        second_contributory_cause NVARCHAR (50) NOT NULL,
+        most_severe_injury NVARCHAR (50) NOT NULL,
+        speedlimit INTEGER NOT NULL,
+        -- te dalej to z vehicle
+        travel_direction NVARCHAR (10),
+        maneuver NVARCHAR (50),
+        first_contact_point NVARCHAR (50)
+    );
+
+-- dim_location
+CREATE TABLE
+    dim_location (
+        location_id BIGINT NOT NULL PRIMARY KEY,
+        street_no INTEGER NOT NULL,
+        street_direction VARCHAR(1) NOT NULL,
+        street_name NVARCHAR (50) NOT NULL,
+        latitude FLOAT NULL,
+        longitude FLOAT NULL
+    );
+
+-- dim_person
+CREATE TABLE
+    dim_person (
+        person_id BIGINT NOT NULL PRIMARY KEY,
+        person_type NVARCHAR (50) NOT NULL,
+        zip_code NVARCHAR (50) NULL,
+        sex VARCHAR(1) NULL,
+        age INTEGER NULL,
+        safety_equipment NVARCHAR (50) NULL,
+        airbag_deployed BIT NULL,
+        ejection NVARCHAR (50) NULL,
+        injury_classification NVARCHAR (50) NULL,
+        driver_action NVARCHAR (50) NULL,
+        driver_vision NVARCHAR (50) NULL,
+        physical_condition NVARCHAR (50) NULL,
+        bac_result NVARCHAR (50) NULL,
+        bac_result_value FLOAT NULL,
+        -- tutaj wlatuje SCD2
+        start_date_id INT NOT NULL,
+        end_date_id INT NOT NULL,
+        isCurrent bit NOT NULL,
+        CONSTRAINT fk_start_date FOREIGN KEY (start_date_id) REFERENCES dim_date (date_id),
+        CONSTRAINT fk_end_date FOREIGN KEY (end_date_id) REFERENCES dim_date (date_id)
+    )
+    --dim_vehicle
+CREATE TABLE
+    dim_vehicle (
+        vehicle_id BIGINT NOT NULL PRIMARY KEY,
+        make NVARCHAR (50) NOT NULL,
+        model NVARCHAR (50) NOT NULL,
+        year INT NOT NULL,
+        defect NVARCHAR (50) NOT NULL,
+        type NVARCHAR (50) NOT NULL
+    )
+CREATE TABLE
+    dim_date (
+        date_id INT NOT NULL PRIMARY KEY, -- Surrogate key (np. 20240507)
+        full_date DATE NOT NULL, -- Pełna data (YYYY-MM-DD)
+        year SMALLINT NOT NULL, -- Rok (np. 2024)
+        quarter TINYINT NOT NULL, -- Kwartał (1-4)
+        month TINYINT NOT NULL, -- Miesiąc (1-12)
+        month_name NVARCHAR (10) NOT NULL, -- Nazwa miesiąca (np. 'May')
+        day_of_month TINYINT NOT NULL, -- Dzień miesiąca (1-31)
+        day_of_week TINYINT NOT NULL, -- Dzień tygodnia (1=Monday, 7=Sunday)
+        day_name NVARCHAR (10) NOT NULL, -- Nazwa dnia tygodnia (np. 'Tuesday')
+        is_weekend BIT NOT NULL, -- Czy to weekend (1/0)
+        week_of_year TINYINT NOT NULL, -- Numer tygodnia w roku (1-53)
+        is_holiday BIT NOT NULL DEFAULT 0, -- Czy to święto (0 domyślnie)
+        holiday_name NVARCHAR (50) NULL -- Nazwa święta jeśli występuje
+    );
+
 -- fact_crash
 CREATE TABLE
     fact_crash (
@@ -26,71 +113,7 @@ CREATE TABLE
         CONSTRAINT fk_person_id FOREIGN KEY (person_id) REFERENCES dim_person (person_id)
     );
 
--- dim_crash_info
-CREATE TABLE
-    dim_crash_info (
-        crash_info_id NVARCHAR (150) NOT NULL PRIMARY KEY,
-        traffic_control_device NVARCHAR (50) NOT NULL,
-        device_condition NVARCHAR (50) NOT NULL,
-        first_crash_type NVARCHAR (50) NOT NULL,
-        trafficway_type NVARCHAR (50) NOT NULL,
-        alignment NVARCHAR (50) NOT NULL,
-        roadway_surface_condition NVARCHAR (50) NOT NULL,
-        road_defect NVARCHAR (50) NOT NULL,
-        report_type NVARCHAR (50) NULL,
-        -- w sensie jest troche missing to null ?
-        crash_type NVARCHAR (50) NOT NULL,
-        damage NVARCHAR (50) NOT NULL,
-        prime_contributory_cause NVARCHAR (50) NOT NULL,
-        second_contributory_cause NVARCHAR (50) NOT NULL,
-        most_severe_injury NVARCHAR (50) NOT NULL,
-        speedlimit INTEGER NOT NULL,
-        -- te dalej to z vehicle
-        travel_direction NVARCHAR (10),
-        maneuver NVARCHAR (50),
-        first_contact_point NVARCHAR (50)
-    );
-
--- dim_location
-CREATE TABLE
-    dim_location (
-        location_id NVARCHAR (150) NOT NULL PRIMARY KEY,
-        street_no INTEGER NOT NULL,
-        street_direction VARCHAR(1) NOT NULL,
-        street_name NVARCHAR (50) NOT NULL,
-        latitude FLOAT NULL,
-        longitude FLOAT NULL
-    );
-
--- dim_person
-CREATE TABLE
-    dim_person (
-        person_id NVARCHAR (150) NOT NULL PRIMARY KEY,
-        person_type NVARCHAR (50) NOT NULL,
-        zip_code NVARCHAR (50) NULL,
-        sex VARCHAR(1) NULL,
-        age INTEGER NULL,
-        safety_equipment NVARCHAR (50) NULL,
-        airbag_deployed BIT NULL,
-        ejection NVARCHAR (50) NULL,
-        injury_classification NVARCHAR (50) NULL,
-        driver_action NVARCHAR (50) NULL,
-        driver_vision NVARCHAR (50) NULL,
-        physical_condition NVARCHAR (50) NULL,
-        bac_result NVARCHAR (50) NULL,
-        bac_result_value FLOAT NULL
-    )
-    --dim_vehicle
-CREATE TABLE
-    dim_vehicle (
-        vehicle_id BIGINT NOT NULL PRIMARY KEY,
-        make NVARCHAR (50) NOT NULL,
-        model NVARCHAR (50) NOT NULL,
-        year INT NOT NULL,
-        defect NVARCHAR (50) NOT NULL,
-        type NVARCHAR (50) NOT NULL
-    )
-    --fact_weather
+--fact_weather
 CREATE TABLE
     fact_weather (
         weather_id BIGINT NOT NULL PRIMARY KEY,
@@ -113,26 +136,10 @@ CREATE TABLE
         solarradiation int NOT NULL,
         solarenergy decimal(3, 2) NOT NULL,
         uvindex int NOT NULL,
-        conditions NVARCHAR (50) NOT NULL
+        conditions NVARCHAR (50) NOT NULL,
+        CONSTRAINT fk_date_id FOREIGN KEY (date_id) REFERENCES dim_date (date_id)
     )
-CREATE TABLE
-    dim_date (
-        date_id INT NOT NULL PRIMARY KEY, -- Surrogate key (np. 20240507)
-        full_date DATE NOT NULL, -- Pełna data (YYYY-MM-DD)
-        year SMALLINT NOT NULL, -- Rok (np. 2024)
-        quarter TINYINT NOT NULL, -- Kwartał (1-4)
-        month TINYINT NOT NULL, -- Miesiąc (1-12)
-        month_name NVARCHAR (10) NOT NULL, -- Nazwa miesiąca (np. 'May')
-        day_of_month TINYINT NOT NULL, -- Dzień miesiąca (1-31)
-        day_of_week TINYINT NOT NULL, -- Dzień tygodnia (1=Monday, 7=Sunday)
-        day_name NVARCHAR (10) NOT NULL, -- Nazwa dnia tygodnia (np. 'Tuesday')
-        is_weekend BIT NOT NULL, -- Czy to weekend (1/0)
-        week_of_year TINYINT NOT NULL, -- Numer tygodnia w roku (1-53)
-        is_holiday BIT NOT NULL DEFAULT 0, -- Czy to święto (0 domyślnie)
-        holiday_name NVARCHAR (50) NULL -- Nazwa święta jeśli występuje
-    );
-
--- KOMENTARZE 
--- moze zastanowic sie czy by nie wywalic czesci kolumn ktore beda bezuzyteczne 
--- - wywalic kolumny gdzie jest duzo missingow ? np intersection_related 
--- - ew zmniejszyc liczbe w NVARCHAR (50) na mniejsza ale idk
+    -- KOMENTARZE 
+    -- moze zastanowic sie czy by nie wywalic czesci kolumn ktore beda bezuzyteczne 
+    -- - wywalic kolumny gdzie jest duzo missingow ? np intersection_related 
+    -- - ew zmniejszyc liczbe w NVARCHAR (50) na mniejsza ale idk
