151be5c1c0988876d89b9dd0af0677b17a32da79
tomaszzywicki <tomasz.zywicki03@gmail.com>
2025-05-19 09:35:39 +0200

    change file structure a bit

--- airflow-postgres-docker/etl/transform/transform_crash.py
+++ airflow-postgres-docker/etl/transform/transform_crash.py
@@ -1,4 +1,10 @@
 
+
+
+
+
+# --te z location
+
 # location_id    - jako sztuczny klucz główny
 # STREET_NO           0 nulli - zajebiście (ale chyba dajemy jako string i wtedy jak null to UNKNOWN) 
 # STREET_DIRECTION    chyba z 4 nulle (też UNKNOWN)
