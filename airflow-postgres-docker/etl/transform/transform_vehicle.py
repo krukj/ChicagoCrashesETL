2a954fa83709a9fe94b9102085e7e843838fa12d
krukj <126516292+krukj@users.noreply.github.com>
2025-05-19 10:34:15 +0200

    transform vehicles

---
+++ airflow-postgres-docker/etl/transform/transform_vehicle.py
@@ -0,0 +1,71 @@
+# CRASH_UNIT_ID - wywalic i stworzyc surrogate key vehicle_id
+# CRASH_RECORD_ID - do polaczenia 
+# CRASH_DATE - wywalic 
+# UNIT_NO - wywalic 
+# UNIT_TYPE - type:str, nazwa: unit_type, nulle -> UNKNOW
+# NUM_PASSENGERS - type: int, nazwa: num_passengers, nulle ->
+# VEHICLE_ID - wwyalic ?
+# CMRC_VEH_I - wywalic
+# MAKE - type: str, nazwa: make, nulle -> UNKNOWN
+# MODEL - type: str, nazwa: model, nulle -> UNKNOWN
+# LIC_PLATE_STATE - wywalic
+# VEHICLE_YEAR - type: int, nazwa: vehicle_year, nulle ->
+# VEHICLE_DEFECT - type: str, nazwa: vehicle_defect, nulle -> UNKNOWN
+# VEHICLE_TYPE - type: str, nazwa: vehicle_type, nulle -> UNKNOWN
+# VEHICLE_USE - type: str, nazwa: vehicle_use, nulle -> UNKNOWN
+# TRAVEL_DIRECTION - type: str, nazwa: travel_direction,  nulle -> UNKNOWN
+# MANEUVER - type: str, nazwa: maneuver, nulle -> UNKNOWN
+# TOWED_I - wywalic
+# FIRE_I - wywalic
+# OCCUPANT_CNT - dodac do fact_crash, type: int, nazwa: occupant_count,
+# EXCEED_SPEED_LIMIT_I - wywalic
+# "TOWED_BY", - to i nizej wywalic
+# "TOWED_TO",
+# "AREA_00_I",
+# "AREA_01_I",
+# "AREA_02_I",
+# "AREA_03_I",
+# "AREA_04_I",
+# "AREA_05_I",
+# "AREA_06_I",
+# "AREA_07_I",
+# "AREA_08_I",
+# "AREA_09_I",
+# "AREA_10_I",
+# "AREA_11_I",
+# "AREA_12_I",
+# "AREA_99_I",
+# FIRST_CONTACT_POINT - type: str, nazwa: first_contact_point, nulle -> UNKNOWN
+# # "CMV_ID", - to i nizej wywalic
+# "USDOT_NO",
+# "CCMC_NO",
+# "ILCC_NO",
+# "COMMERCIAL_SRC",
+# "GVWR",
+# "CARRIER_NAME",
+# "CARRIER_STATE",
+# "CARRIER_CITY",
+# "HAZMAT_PLACARDS_I",
+# "HAZMAT_NAME",
+# "UN_NO",
+# "HAZMAT_PRESENT_I",
+# "HAZMAT_REPORT_I",
+# "HAZMAT_REPORT_NO",
+# "MCS_REPORT_I",
+# "MCS_REPORT_NO",
+# "HAZMAT_VIO_CAUSE_CRASH_I",
+# "MCS_VIO_CAUSE_CRASH_I",
+# "IDOT_PERMIT_NO",
+# "WIDE_LOAD_I",
+# "TRAILER1_WIDTH",
+# "TRAILER2_WIDTH",
+# "TRAILER1_LENGTH",
+# "TRAILER2_LENGTH",
+# "TOTAL_VEHICLE_LENGTH",
+# "AXLE_CNT",
+# "VEHICLE_CONFIG",
+# "CARGO_BODY_TYPE",
+# "LOAD_TYPE",
+# "HAZMAT_OUT_OF_SERVICE_I",
+# "MCS_OUT_OF_SERVICE_I",
+# "HAZMAT_CLASS",
