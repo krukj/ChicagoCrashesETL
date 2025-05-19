72c3bf54b5a517e90af5c0e977eb9a6a3ef027c0
krukj <126516292+krukj@users.noreply.github.com>
2025-05-19 09:05:22 +0200

    transform weather

--- airflow-postgres-docker/etl/transform/transform_fact_weather.py
+++ airflow-postgres-docker/etl/transform/transform_fact_weather.py
@@ -1,1 +1,24 @@
-# tutaj to chyba
+# "name",
+# datetime 
+# temp: type: float, nazwa: temp
+# feelslike: type: float, nazwa: feels_like
+# dew: type: float, nazwa: dew
+# humidity: type: float, nazwa: humidity
+# precip: type: float, nazwa: precip
+# precipprob: type: int, nazwa: precip_prob, chyba ma tylko distinct 0 i 100
+# preciptype: type: str, nazwa: precip_type, nulle jako NONE moze zeby bylo ze nie bylo opadu 
+# snow: type: 
+# snowdepth: 
+# windgust:
+# windspeed:
+# winddir: jest w katach, zamienic na kierunek typu N, S, type: str, nazwa: wind_dir
+# sealevelpressure: 
+# cloudcover
+"visibility",
+# solarradiation - 
+# solarenergy - wywalic
+# uvindex - wywalic
+# "severerisk",
+"conditions",
+# icon - wywalic
+# stations - wywalic
